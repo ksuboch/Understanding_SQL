1918c44caa1a8b1b33ee53a297dc733a123dddbb
Kirill Suboch <3dvapper.inbox@gmail.com>
2017-12-25 10:39:59 +0400

    'SanJose' -> 'San Jose'

--- ch001_creating_database.sql
+++ ch001_creating_database.sql
@@ -46,7 +46,7 @@ CREATE TABLE dbo.Customers (
 INSERT INTO dbo.Customers(Cnum, Cname, City, Rating, Snum) VALUES
 	(2001, 'Hoffman' , 'London'  , 100, 1001),
 	(2002, 'Giovanni', 'Rome'    , 200, 1003),
-	(2003, 'Liu'     , 'SanJose' , 200, 1002),
+	(2003, 'Liu'     , 'San Jose' , 200, 1002),
 	(2004, 'Grass'   , 'Berlin'  , 300, 1002),
 	(2006, 'Clemens' , 'London'  , 100, 1001),
 	(2008, 'Cisneros', 'San Jose', 300, 1007),
